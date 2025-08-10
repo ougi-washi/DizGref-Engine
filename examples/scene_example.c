393cd1f9a9d22c5a33ade4f5b641881a56378a91
ougi-washi <jedfakhfekh@gmail.com>
2025-08-11 03:21:35 +0900

    Add scene_example

---
+++ examples/scene_example.c
@@ -0,0 +1,39 @@
+// Syphax-Engine - Ougi Washi
+
+#include "se_scene.h"
+
+#define WIDTH 1920
+#define HEIGHT 1080
+
+i32 main() {
+    se_render_handle render_handle = {0};
+    
+    se_window* window = se_window_create("Syphax-Engine - Scene Example", WIDTH, HEIGHT);
+
+    se_scene_2d scene_2d = {0};
+    se_scene_2d_add_render_buffer(&scene_2d, se_render_buffer_create(&render_handle, WIDTH, HEIGHT));
+    
+    se_scene_3d scene_3d = {0};
+
+    se_shaders_ptr model_shaders = {0};
+    se_shaders_ptr_add(&model_shaders, se_shader_load(&render_handle, "vert.glsl", "frag_mesh.glsl"));
+    se_scene_3d_add_model(&scene_3d, se_model_load_obj(&render_handle, "cube.obj", &model_shaders));
+    se_scene_3d_set_camera(&scene_3d, se_camera_create(&render_handle));
+    
+    key_combo exit_keys = {0};
+    key_combo_add(&exit_keys, GLFW_KEY_ESCAPE);
+
+    while (!se_window_should_close(window)) {
+        se_window_poll_events();
+        se_window_check_exit_keys(window, &exit_keys);
+        
+        se_scene_3d_render(&scene_3d, &render_handle);
+        se_scene_2d_render(&scene_2d, &render_handle, window);
+        
+        se_window_render_screen(window);
+    }
+
+    se_render_handle_cleanup(&render_handle);
+    se_window_destroy(window);
+    return 0;
+}
