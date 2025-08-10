969ea9450f15d599ff9001a08f467c91479d7197
ougi-washi <jedfakhfekh@gmail.com>
2025-08-10 14:11:30 +0900

    Add se_scene header and cpp

---
+++ src/se_scene.h
@@ -0,0 +1,14 @@
+// Syphax-Engine - Ougi Washi
+
+#ifndef SE_SCENE_H
+#define SE_SCENE_H
+
+#include "se_render.h"
+
+typedef struct {
+} se_scene2d;
+
+typedef struct {
+} se_scene3do;
+
+#endif // SE_SCENE_H
